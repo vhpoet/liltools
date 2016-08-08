61fb5f5b83c93ac17be36884be564327f762f65c
Vahe Hovhannisyan <vhpoet@gmail.com>
2016-08-07 23:31:08 -0700

    .

---
+++ index.js
@@ -0,0 +1,58 @@
+const app = require('koa')()
+const router = require('koa-router')()
+const serve = require('koa-static')
+const Xray = require('x-ray')
+const x = Xray()
+
+app.use(serve('html'))
+
+const getUser = (username) => {
+  return new Promise((resolve, reject) => {
+    x('http://instagram.com/' + username, {
+      scripts: ['script']
+    })((err, content) => {
+      try {
+        content.scripts.forEach((script) => {
+          if (script.indexOf('window._sharedData') > -1) {
+            script = script.replace('window._sharedData =', '')
+            script = script.slice(0, -1)
+
+            resolve(JSON.parse(script).entry_data.ProfilePage[0].user)
+          }
+        })
+      } catch (e) {
+        reject()
+      }
+    })
+  })
+}
+
+router.get('/get', function *() {
+  const user = yield getUser(this.query.username)
+
+  this.body = '<div>ID: ' + user.id + '</div><br /><div><a href="json/' + this.query.username + '" target="_parent">JSON</a></div>'
+})
+
+router.get('/json/:username', function *(next) {
+  const user = yield getUser(this.params.username)
+
+  this.body = {
+    id: user.id,
+    username: user.username,
+    follows: user.follows.count,
+    followed_by: user.followed_by.count,
+    profile_pic_url: user.profile_pic_url,
+    profile_pic_url_hd: user.profile_pic_url_hd,
+    biography: user.biography,
+    full_name: user.full_name,
+    media_count: user.media.count,
+    external_url: user.external_url,
+    is_private: user.is_private
+  }
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3120)
